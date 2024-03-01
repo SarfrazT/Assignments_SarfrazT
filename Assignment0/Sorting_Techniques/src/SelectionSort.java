4fbff0bee982fa10ff383db0854894e4c36d243d
Sarfrajnawaz.TahasildarM <sarfrajnawaz.tahasildarM@cognizant.com>
2024-03-01 10:52:17 +0530

    Initial commit

---
+++ Assignment0/Sorting_Techniques/src/SelectionSort.java
@@ -0,0 +1,19 @@
+public class SelectionSort {
+
+    //create logic for selection sort technique for sorting array
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+}
